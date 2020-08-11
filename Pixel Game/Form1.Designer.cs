6dffe6fe53ece3493fb3eaead6ad80546a167194
Mass <klauscoding@gmail.com>
2020-08-11 14:16:36 +0000

    Add project files.

---
+++ Pixel Game/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Pixel_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ball = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.enemy = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ball
+            // 
+            this.ball.BackColor = System.Drawing.Color.Red;
+            this.ball.Location = new System.Drawing.Point(388, 249);
+            this.ball.Name = "ball";
+            this.ball.Size = new System.Drawing.Size(12, 13);
+            this.ball.TabIndex = 0;
+            this.ball.TabStop = false;
+            this.ball.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.Black;
+            this.pictureBox2.Location = new System.Drawing.Point(50, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(5000, 500);
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.PictureBox1_Click);
+            // 
+            // enemy
+            // 
+            this.enemy.BackColor = System.Drawing.Color.White;
+            this.enemy.Location = new System.Drawing.Point(739, 186);
+            this.enemy.Name = "enemy";
+            this.enemy.Size = new System.Drawing.Size(12, 127);
+            this.enemy.TabIndex = 1;
+            this.enemy.TabStop = false;
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.White;
+            this.player.Location = new System.Drawing.Point(38, 186);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(12, 127);
+            this.player.TabIndex = 2;
+            this.player.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(781, 500);
+            this.Controls.Add(this.player);
+            this.Controls.Add(this.enemy);
+            this.Controls.Add(this.ball);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Painting);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Movement_Trigger);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Movement_Cancel);
+            ((System.ComponentModel.ISupportInitialize)(this.ball)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.enemy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox ball;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox enemy;
+        private System.Windows.Forms.PictureBox player;
+    }
+}
+
---
+++ Pixel Game/Form1.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pixel_Game
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern int AllocConsole();
+
+        public bool Up, Down;
+        Task t;
+        Point playerpos;
+        Point enemypos;
+        bool bounce;
+        bool sideWaysDown;
+        private bool sideWaysUp;
+        private bool colision;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            colision = sideWaysUp = sideWaysDown = bounce = Up = Down = false;
+
+            playerpos = player.Location;
+            enemypos = enemy.Location;
+            t = Task.Run(() => GameLoop());
+
+            Console.WriteLine("test");
+        }
+
+        private void GameLoop()
+        {
+            while (true)
+            {
+                DetectColision();
+                Movement();
+
+                Thread.Sleep(5);
+            }
+        }
+
+        private void DetectColision()
+        {
+            if (ball.Location.X < player.Location.X + player.Width && ball.Location.X + ball.Width > player.Location.X + 2 && ball.Location.Y + ball.Height > player.Location.Y && player.Location.Y + player.Height > ball.Location.Y)
+            {
+                bounce = true;
+                colision = true;
+
+                if (Up)
+                {
+                    sideWaysDown = false;
+                    sideWaysUp = true;
+                }
+
+                if (Down)
+                {
+                    sideWaysDown = true;
+                    sideWaysUp = false;
+                }
+
+            }
+            else if (ball.Location.X + ball.Width > enemy.Location.X && ball.Location.X + ball.Width < enemy.Location.X + 2 && ball.Location.Y + ball.Height > enemy.Location.Y && enemy.Location.Y + enemy.Height > ball.Location.Y)
+            {
+                bounce = false;
+                colision = true;
+
+                if (UpEn)
+                {
+                    sideWaysDown = false;
+                    sideWaysUp = true;
+                }
+                
+                if(DownEn)
+                {
+                    sideWaysDown = true;
+                    sideWaysUp = false;
+                }
+
+            }
+            else
+            {
+                colision = false;
+            }
+
+            if (ball.Location.Y <= 0 || ball.Location.Y >= 500)
+            {
+                y = -y;
+            }
+        }
+
+        delegate void SetTextCallback(Form f, PictureBox ctrl, Point position);
+        public static void SetPos(Form form, PictureBox ctrl, Point position)
+        {
+            if (ctrl.InvokeRequired)
+            {
+                SetTextCallback p = new SetTextCallback(SetPos);
+                try
+                {
+                    form.Invoke(p, new object[] { form, ctrl, position });
+                }
+                catch (Exception)
+                {
+
+                }
+            }
+            else
+            {
+                ctrl.Location = position;
+            }
+        }
+
+        private void Movement()
+        {
+            if (Up)
+            {
+                playerpos.Y--;
+            }
+
+            if (Down)
+            {
+                playerpos.Y++;
+            }
+
+            if (UpEn)
+            {
+                enemypos.Y--;
+            }
+
+            if (DownEn)
+            {
+                enemypos.Y++;
+            }
+
+            SetPos(this, player, playerpos);
+            SetPos(this, enemy, enemypos);
+            MoveBall();
+        }
+
+        int y = 0;
+        private bool DownEn;
+        private bool UpEn;
+
+        private void MoveBall()
+        {
+            Point p;
+            if (colision)
+            {
+                if (sideWaysUp)
+                {
+                    y = -1;
+                }
+                else if (sideWaysDown)
+                {
+                    y = 1;
+                }
+            }
+
+            if (bounce)
+            {
+                p = new Point(ball.Location.X + 1, ball.Location.Y + y);
+            }
+            else
+            {
+                p = new Point(ball.Location.X - 1, ball.Location.Y + y);
+            }
+
+
+
+            SetPos(this, ball, p);
+        }
+
+        private void PictureBox1_Click(object sender, EventArgs e)
+        {
+
+
+        }
+
+        private void Painting(object sender, PaintEventArgs e)
+        {
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Movement_Trigger(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.W:
+                    Up = true;
+                    break;
+                case Keys.S:
+                    Down = true;
+                    break;
+                case Keys.Up:
+                    UpEn = true;
+                    break;
+                case Keys.Down:
+                    DownEn = true;
+                    break;
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+        private void Movement_Cancel(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.W:
+                    Up = false;
+                    break;
+                case Keys.S:
+                    Down = false;
+                    break;
+                case Keys.Up:
+                    UpEn = false;
+                    break;
+                case Keys.Down:
+                    DownEn = false;
+                    break;
+            }
+        }
+    }
+}
